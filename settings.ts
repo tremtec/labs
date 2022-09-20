5bb24cb058e69a83a59d1f9bc49d3cfa4312c725
Marco Antônio <ma.souza.junior@gmail.com>
2022-09-20 11:31:05 -0300

    fix: typo

--- settings.ts
+++ settings.ts
@@ -1,7 +1,7 @@
 export const site = {
   name: "TremTec",
   title: "TremTec.labs",
-  subTitle: "Welcome to you base `fresh` project",
+  subTitle: "Welcome to your base `fresh` project",
   description: "TremTec Labs",
   keywords: "TremTec,software,learning,projects",
   repository: "https://github.com/tremtec/labs",
