d7bf70152ff186176fdfabbf073363adb504bbf4
Marco Antônio <ma.souza.junior@gmail.com>
2022-10-09 22:10:13 -0300

    enh: update callback response

--- routes/api/auth/github/callback.ts
+++ routes/api/auth/github/callback.ts
@@ -4,12 +4,18 @@ export const handler: Handlers = {
   GET(req: Request, ctx: HandlerContext) {
     console.log({ req, ctx });
     const qs = new URLSearchParams(req.url)
-    return new Response(JSON.stringify(qs.getAll()));
+    return new Response(JSON.stringify({
+      error: qs.get('error'),
+      error_description: qs.get('error_description'),
+    }));
   },
 
   POST(req: Request, ctx: HandlerContext) {
     console.log({ req, ctx });
     const qs = new URLSearchParams(req.url)
-    return new Response(JSON.stringify(qs.getAll()));
+    return new Response(JSON.stringify({
+      error: qs.get('error'),
+      error_description: qs.get('error_description'),
+    }));
   },
 };
