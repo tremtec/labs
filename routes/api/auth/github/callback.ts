393a199168e762a2a282babe5b594d9f4e08d708
Marco Antônio <ma.souza.junior@gmail.com>
2022-10-09 22:07:00 -0300

    enh: update callback response

--- routes/api/auth/github/callback.ts
+++ routes/api/auth/github/callback.ts
@@ -3,11 +3,13 @@ import { HandlerContext, Handlers } from "$fresh/server.ts";
 export const handler: Handlers = {
   GET(req: Request, ctx: HandlerContext) {
     console.log({ req, ctx });
-    return new Response("GET: Hello form github callback");
+    const qs = new URLSearchParams(req.url)
+    return new Response(JSON.stringify(qs.getAll()));
   },
 
   POST(req: Request, ctx: HandlerContext) {
     console.log({ req, ctx });
-    return new Response("POST: Hello form github callback");
+    const qs = new URLSearchParams(req.url)
+    return new Response(JSON.stringify(qs.getAll()));
   },
 };
