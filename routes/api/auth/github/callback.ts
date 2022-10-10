19258c09b77f8f7f1fea8dfaeb832389b0040b7e
Marco Antônio <ma.souza.junior@gmail.com>
2022-10-09 22:13:32 -0300

    enh: cleanup callback URL

--- routes/api/auth/github/callback.ts
+++ routes/api/auth/github/callback.ts
@@ -1,21 +1,15 @@
-import { HandlerContext, Handlers } from "$fresh/server.ts";
+import { Handler } from "$fresh/server.ts";
 
-export const handler: Handlers = {
-  GET(req: Request, ctx: HandlerContext) {
-    console.log({ req, ctx });
-    const qs = new URLSearchParams(req.url)
-    return new Response(JSON.stringify({
-      error: qs.get('error'),
-      error_description: qs.get('error_description'),
-    }));
-  },
-
-  POST(req: Request, ctx: HandlerContext) {
-    console.log({ req, ctx });
-    const qs = new URLSearchParams(req.url)
-    return new Response(JSON.stringify({
-      error: qs.get('error'),
-      error_description: qs.get('error_description'),
-    }));
-  },
+export const handler: Handler = (req, ctx) => {
+  console.log({ req, ctx });
+  const qs = new URLSearchParams(req.url);
+  return new Response(
+    JSON.stringify({
+      code: qs.get("code"),
+      state: qs.get("state"),
+      // WIP: save code & state on cookies
+      error: qs.get("error"),
+      error_description: qs.get("error_description"),
+    }),
+  );
 };
