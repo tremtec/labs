a39c72a1ae4c65f0819793485d3f20d9c8f05774
Marco Antônio <ma.souza.junior@gmail.com>
2023-06-06 12:52:04 -0300

    enh: use current color for tt logo

--- packages/icon/TremTecLogo.tsx
+++ packages/icon/TremTecLogo.tsx
@@ -1,8 +1,8 @@
-import { JSX } from "https://esm.sh/v94/preact@10.11.0/jsx-runtime/src/index.d.ts";
+import { h } from "preact";
 
 const LOGO_SIZE = 40;
 
-interface Props extends JSX.SVGAttributes<SVGSVGElement> {
+interface Props extends h.JSX.SVGAttributes<SVGSVGElement> {
   // size in pixels
   size?: number;
 }
@@ -25,30 +25,25 @@ export default function TremTecLogo({
           cx="130.5"
           cy="136.5"
           r="101.5"
-          stroke="#0B1427"
+          stroke="currentColor"
           strokeWidth="12"
         />
         <path
           d="M0 128.978L12.146 111.693L128.467 90.2044L118.19 106.088L78.0146 114.029V182.234L64.4671 185.036V116.832L15.4161 125.708L0 128.978Z"
-          fill="#0B1527"
-          stroke="#353535"
+          fill="currentColor"
+          stroke="currentColor"
         />
         <path
           d="M127.066 104.686L139.212 87.4015L255.533 65.9124L245.255 81.7956L205.08 89.7372V157.942L191.533 160.745V92.5402L142.482 101.416L127.066 104.686Z"
-          fill="#0B1527"
-          stroke="#353535"
+          fill="currentColor"
+          stroke="currentColor"
         />
         <path
           d="M0 218.204L12.6131 200.453L256 154.672L245.723 170.555L0 218.204Z"
-          fill="#0B1427"
-          stroke="#353535"
+          fill="currentColor"
+          stroke="currentColor"
         />
       </g>
-      <defs>
-        <clipPath id="clip0">
-          <rect width="256" height="256" rx="20" fill="white" />
-        </clipPath>
-      </defs>
     </svg>
   );
 }
