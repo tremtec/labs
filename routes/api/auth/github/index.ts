44583e95f28c2ad648ce668464a887792d4b19f8
Marco Antônio <ma.souza.junior@gmail.com>
2023-06-12 12:39:33 -0300

    fix: update logs

--- routes/api/auth/github/index.ts
+++ routes/api/auth/github/index.ts
@@ -2,11 +2,12 @@ import { HandlerContext, Handlers } from "$fresh/server.ts";
 import { GitHubObject } from "#/routes/api/auth/github/client.ts";
 import * as log from "https://deno.land/std@0.191.0/log/mod.ts";
 
+const logger = log.getLogger("auth")
 
 export const handler: Handlers = {
   GET(req: Request, ctx: HandlerContext) {
     const redirectLink = GitHubObject.code.createLink();
-    log.info({ req, ctx, redirectLink });
+    logger.info({ req, ctx, redirectLink });
     return Response.redirect(redirectLink);
   },
 
