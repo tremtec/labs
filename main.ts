8d47d3cf44cce69644246d01f6570dd014596ddd
Marco Antônio <ma.souza.junior@gmail.com>
2023-07-02 10:59:25 -0300

    fea: setup logger

--- main.ts
+++ main.ts
@@ -5,10 +5,24 @@
 /// <reference lib="deno.ns" />
 /// <reference lib="deno.unstable" />
 
+import * as log from "$std/log/mod.ts";
 import { Manifest, start } from "$fresh/server.ts";
 import manifest from "#/fresh.gen.ts";
 
 import twindPlugin from "$fresh/plugins/twind.ts";
 import twindConfig from "#/twind.config.ts";
+
+log.setup({
+  handlers: {
+    console: new log.handlers.ConsoleHandler("DEBUG"),
+  },
+
+  loggers: {
+    default: {
+      level: "DEBUG",
+      handlers: ["console"],
+    },
+  },
+});
 
 await start(manifest as Manifest, { plugins: [twindPlugin(twindConfig)] });
