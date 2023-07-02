91b018095dca3952dfe6f94bb8a58fa7d417cdb8
Marco Antônio <ma.souza.junior@gmail.com>
2023-07-02 11:33:38 -0300

    fix: setup logger

--- packages/shared/logging.ts
+++ packages/shared/logging.ts
@@ -1,3 +1,16 @@
 import * as log from "$std/log/mod.ts";
 
+log.setup({
+  handlers: {
+    console: new log.handlers.ConsoleHandler("DEBUG"),
+  },
+  loggers: {
+    // configure default logger available via short-hand methods above
+    default: {
+      level: "DEBUG",
+      handlers: ["console"],
+    }
+  },
+});
+
 export const logger = log.getLogger();
