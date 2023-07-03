4ef4cb4d72f69750db644699a61010458862b358
Marco Antônio <ma.souza.junior@gmail.com>
2023-07-03 19:38:36 -0300

    enh: add wip component

---
+++ packages/components/WorkInProgress.tsx
@@ -0,0 +1,8 @@
+export default function WorkInProgress() {
+  return (
+    <div class="animate-pulse text-center">
+      <h2 class="text-4xl">🚧</h2>
+      <h3 class="text-md">Work in progress...</h3>
+    </div>
+  );
+}
--- routes/app/interviews.tsx
+++ routes/app/interviews.tsx
@@ -1,12 +1,15 @@
 import AppLayout from "~/components/layouts/AppLayout.tsx";
 
 import { PageProps } from "$fresh/server.ts";
+import WorkInProgress from "~/components/WorkInProgress.tsx";
 
 export default function Interviews(props: PageProps) {
   return (
     <AppLayout path={props.url.pathname}>
-      <div class="text-left">
-        <h1 class="text-3xl">🧾 Interviews</h1>
+      <div class="text-left grid gap-8">
+        <h1 class="text-3xl">Interviews</h1>
+
+        <WorkInProgress />
       </div>
     </AppLayout>
   );
