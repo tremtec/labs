0fa33c059343abbd8b25011af482dadee6a0ab95
Marco Antônio <ma.souza.junior@gmail.com>
2023-07-23 17:29:54 -0300

    enh: remove deps on date-fns

--- packages/shared/date.ts
+++ packages/shared/date.ts
@@ -1,6 +1,21 @@
-import { formatDistance } from "date-fns"
+import { difference } from "$std/datetime/mod.ts";
 
 export const now = () => new Date();
+
+function formatDistance(start: Date, end: Date) {
+  const diff = difference(start, end);
+
+  for (const [unit, value] of Object.entries(diff).toReversed()) {
+    const parsedUnit = unit.slice(
+      0,
+      unit.length - (value === 1 ? 1 : 0),
+    );
+    if (value > 0) return `at ${value} ${parsedUnit} ago`;
+    continue;
+  }
+
+  return "now";
+}
 
 export function dateFormatter(
   date = now(),
