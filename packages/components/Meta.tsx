e3ce4b446a6e7129cc6f7267870dcb4eb5994d74
Marco Antônio <ma.souza.junior@gmail.com>
2023-08-19 13:55:24 -0300

    fix: add viewport tag

--- packages/components/Meta.tsx
+++ packages/components/Meta.tsx
@@ -5,6 +5,7 @@ export function Meta() {
   return (
     <Head>
       <title>{site.title}</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta name="description" content={site.description} />
       <meta name="keywords" content={site.keywords} />
     </Head>
