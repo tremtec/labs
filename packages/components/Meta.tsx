3ffcd52441ced8761b313f2721be161f7bb23251
Marco Antônio <ma.souza.junior@gmail.com>
2023-08-29 12:56:26 -0300

    fea: setup daisy ui

--- packages/components/Meta.tsx
+++ packages/components/Meta.tsx
@@ -5,12 +5,27 @@ export function Meta() {
   return (
     <Head>
       <title>{site.title}</title>
-      
+
       <meta charSet="utf-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
       <meta name="description" content={site.description} />
       <meta name="keywords" content={site.keywords} />
+
+      <DaisyUI />
     </Head>
+  );
+}
+
+function DaisyUI() {
+  return (
+    <>
+      <link
+        href="https://cdn.jsdelivr.net/npm/daisyui@3.6.3/dist/full.css"
+        rel="stylesheet"
+        type="text/css"
+      />
+      <script src="https://cdn.tailwindcss.com"></script>
+    </>
   );
 }
