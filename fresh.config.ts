c898553593a30ddab60931259b26d1b70ccc092d
Marco Antônio <ma.souza.junior@gmail.com>
2023-08-30 19:55:58 -0300

    fea: add daisy-ui with fresh-plugin

--- fresh.config.ts
+++ fresh.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from "$fresh/server.ts";
 import twindPlugin from "$fresh/plugins/twind.ts";
 import twindConfig from "#/twind.config.ts";
+import { setupDaisy } from "#/plugins/daisy.ts";
 
 export default defineConfig({
-  plugins: [twindPlugin(twindConfig)],
+  plugins: [
+    twindPlugin(twindConfig),
+    await setupDaisy(),
+  ],
 });
--- packages/components/Meta.tsx
+++ packages/components/Meta.tsx
@@ -11,21 +11,7 @@ export function Meta() {
 
       <meta name="description" content={site.description} />
       <meta name="keywords" content={site.keywords} />
-
-      <DaisyUI />
     </Head>
   );
 }
 
-function DaisyUI() {
-  return (
-    <>
-      <link
-        href="https://cdn.jsdelivr.net/npm/daisyui@3.6.3/dist/full.css"
-        rel="stylesheet"
-        type="text/css"
-      />
-      <script src="https://cdn.tailwindcss.com"></script>
-    </>
-  );
-}
