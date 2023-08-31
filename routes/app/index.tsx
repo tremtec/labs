23b0afff00fd46dd369a2fdd76c6b26d5a917487
Marco Antônio <ma.souza.junior@gmail.com>
2023-08-30 21:28:04 -0300

    fea: use card for dashboard home

--- routes/app/index.tsx
+++ routes/app/index.tsx
@@ -1,30 +1,42 @@
 import { client } from "~/services/github.ts";
 import { defineRoute } from "$fresh/server.ts";
+import GithubIcon from "~/icon/GitHubIcon.tsx";
 
 export default defineRoute(async (req) => {
   const userProfile = await client.fetchAuthenticatedUser(req);
   return (
-    <div class="grid gap-8 text-center sm:text-left sm:grid-cols-3">
-      <img
-        width={240}
-        height={240}
-        class="rounded-full mx-auto"
-        alt={`Welcome ${userProfile.name} 🎉`}
-        src={userProfile.avatarUrl}
-      />
+    <div>
+      <div class="card sm:card-side bg-base-100 shadow-xl text-left">
+        <figure>
+          <img
+            alt={`Welcome ${userProfile.name} 🎉`}
+            src={userProfile.avatarUrl}
+          />
+        </figure>
 
-      <div class="sm:col-span-2 flex flex-col gap-2">
-        <h1 class="text-3xl">
-          Welcome {userProfile.name} 🎉
-        </h1>
+        <div class="card-body">
+          <h1 class="text-3xl">
+            <p>Welcome</p>
+            <p>{userProfile.name} 🎉</p>
+          </h1>
 
-        <h2 class="text-gray-600">
-          @{userProfile.username}
-        </h2>
+          <h2 class="text-gray-600">
+            @{userProfile.username}
+          </h2>
 
-        <div>
-          <p class="py-2 text-gray-500">About you</p>
-          {userProfile.bio.split("\n").map((p) => <p class="px-2">{p}</p>)}
+          <div>
+            <p class="py-2 text-gray-500">About you</p>
+            {userProfile.bio}
+          </div>
+
+          <div class="card-actions justify-end">
+            <a
+              class="btn btn-ghost"
+              href={`https://github.com/${userProfile.username}`}
+            >
+              <GithubIcon />
+            </a>
+          </div>
         </div>
       </div>
     </div>
